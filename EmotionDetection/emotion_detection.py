945203ff769da39db27bbc88464b6a17533e4d45
JVM00 <vargasmoreno.javier@yahoo.com>
2024-12-17 15:39:47 -0500

    Third Commit

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,5 +1,5 @@
 import requests  # Import the requests library to handle HTTP requests
-
+import json
 def emotion_detector(text_to_analyse):  # Define a function named sentiment_analyzer that takes a string input (text_to_analyse)
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'  # URL of the sentiment analysis service
     myobj = { "raw_document": { "text": text_to_analyse } }  # Create a dictionary with the text to be analyzed
